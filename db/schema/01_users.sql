0dd11021f2d8b1af105488f31294c557c06d0033
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 18:22:49 +0000

    initial commit for users table schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,5 +3,8 @@
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  phone_number VARCHAR,
+  email TEXT,
+  password TEXT
 );
