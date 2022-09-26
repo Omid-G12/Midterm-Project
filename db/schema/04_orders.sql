1d0add7579dbe601b4dd42bee6b27a644e20af92
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 19:10:56 +0000

    timestamp default set to Now()

--- db/schema/04_orders.sql
+++ db/schema/04_orders.sql
@@ -5,7 +5,7 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   total INTEGER NOT NULL DEFAULT 0,
-  time_created TIMESTAMP,
+  time_created TIMESTAMP NOT NULL DEFAULT Now(),
   duration INTEGER,
   is_complete BOOLEAN NOT NULL DEFAULT FALSE,
   rating INTEGER
