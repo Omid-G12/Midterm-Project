99609ddda7a1ebe59dbd815fc546eb8e985bc20b
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 21:49:48 +0000

    sample seeds for users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone_number, email, password) VALUES ('Alice', '7781231234', 'alice@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, phone_number, email, password) VALUES ('Kira', '6041231234', 'kira@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
