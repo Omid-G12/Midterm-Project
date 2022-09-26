cb3174d83331ae454828522bdad2a0dda0b119ee
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 22:52:30 +0000

    seeds for ordered_items table

---
+++ db/seeds/03_ordered_items.sql
@@ -0,0 +1,3 @@
+INSERT INTO ordered_items (menu_item_id, user_id, quantity) VALUES (1, 1, 1);
+INSERT INTO ordered_items (menu_item_id, user_id, quantity) VALUES (2, 1, 1);
+INSERT INTO ordered_items (menu_item_id, user_id, quantity) VALUES (3, 1, 1);
