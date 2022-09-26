498ae0f0347aa919400df06ec2f0bf3320865f0e
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 18:38:40 +0000

    users table schema completed

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,10 +1,11 @@
 -- Drop and recreate Users table (Example)
+-- Phone_number stored as VARCHAR since regular phone numbers will exceed the INTEGER limit
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   phone_number VARCHAR,
-  email TEXT,
-  password TEXT
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
