dee76d43f17dafcef6a30caf0cd8cd5b6e3988bf
Omid Ghahramani <omid.12.ghari@gmail.com>
2022-09-26 18:54:57 +0000

    ordered_items table created

---
+++ db/schema/03_ordered_items.sql
@@ -0,0 +1,9 @@
+-- Drop and recreate ordered_items (Example)
+
+DROP TABLE IF EXISTS ordered_items CASCADE;
+CREATE TABLE ordered_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  menu_item_id INTEGER REFERENCES menu_items(id) ON DELETE CASCADE,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL DEFAULT 1
+);
