04a0fbdb6cc229d79ba017fd6a431cb08cdee33c
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-29 04:31:46 +0000

    Wrote JQuery for addtocart

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,11 @@
 // Client facing scripts here
+
+console.log("yes");
+
+$(document).ready(function() {
+  console.log("ready");
+  $('.add-to-cart').click(function(){
+    let quantity = $('.quantity').val();
+    console.log(quantity);
+  });
+});
