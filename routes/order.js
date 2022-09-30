4af06da1be43e8b64323328f996613fc01598110
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-30 01:01:30 +0000

    random order_id

--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,7 @@
 const database = require("../db/queries/database");
 const express = require('express');
 const router  = express.Router();
-let q = 4;
+let pastNumbers = [1, 2, 3 ,4 , 5];
 
 router.get ("/", (req, res) => {
   database.getOrderItems()
@@ -20,28 +20,32 @@ router.get ("/allmenuitems", (req, res) => {
 router.post ("/", (req, res) => {
   //console.log('test', req.body.items);
   const items = req.body.items;
-  console.log("req.body", req.body);
-
-  for (let item of items) {
-    console.log(item);
-    database.getMenuItemsById(item.id)
-    .then (data => {
-      //console.log(data);
-      const orderItem = {
-        order_id: q,
-        menu_item_id: data.id,
-        user_id: req.session.userId,
-        quantity: item.quantity
+  const q = Math.floor(Math.random() * 100);
+  if (!pastNumbers.includes(q)) {
+    for (let item of items) {
+        console.log(item);
+        database.getMenuItemsById(item.id)
+        .then (data => {
+          //console.log(data);
+          const orderItem = {
+            order_id: q,
+            menu_item_id: data.id,
+            user_id: req.session.userId,
+            quantity: item.quantity
+          }
+          console.log(orderItem);
+          database.addOrderItem(orderItem)
+          .then (data => {
+            console.log('added to order_items');
+          })
+        })
       }
-      console.log(orderItem);
-      database.addOrderItem(orderItem)
-      .then (data => {
-        console.log('added to order_items');
-        res.redirect("/checkout/:q");
-      })
-    })
+    pastNumbers.push(q);
+  } else {
+    q += 1;
   }
+
   //addOrderItem(req.body)
-  q += 1;
+  //q += 1;
 })
 module.exports = router;
