eb4a119f9af499111c8e5cab843ec449e95a98da
Hani Siddiqui <hani.siddiqui@gmail.com>
2022-09-30 05:32:28 +0000

    Commit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -34,20 +34,6 @@ $(document).ready(function() {
     //const test = getMenuItems();
     //$.ajax('/api/order/allmenuitems')
     console.log("5555555");
-    // $.ajax('/api/order', {
-    //   method: 'post',
-    //   data: {items: ordered_items},
-    //   success: function(data, textStatus) {
-    //     if (data.redirect) {
-    //         // data.redirect contains the string URL to redirect to
-    //         window.location.href = data.redirect;
-    //     } else {
-    //         // data.form contains the HTML for the replacement form
-    //         // $("#myform").replaceWith(data.form);
-    //     }
-    // }
-    // })
-
   })
 });
 
